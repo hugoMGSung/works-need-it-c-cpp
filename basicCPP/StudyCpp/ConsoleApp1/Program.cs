376c47d9249b995b2ed56805e2a31b5be397de14
Hugo MG Sung <personar95@naver.com>
2025-05-15 09:07:50 +0900

    C++ 2020 저장소 복구

--- basicCPP/StudyCpp/ConsoleApp1/Program.cs
+++ basicCPP/StudyCpp/ConsoleApp1/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Solution s = new Solution();
+            string input = "Zbcdefg";
+            string result = s.solution(input);
+            Console.WriteLine(input);
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string s)
+        {
+            string answer = "";
+            char[] charArray = s.ToCharArray();
+            System.Array.Reverse(charArray);
+            answer = new string(charArray);
+            return answer;
         }
     }
 }
