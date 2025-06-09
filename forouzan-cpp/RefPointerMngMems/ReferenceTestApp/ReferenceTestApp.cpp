4bef81fd65a6edc23ab8d4d10e0f3c9ffeb8825d
Hugo MG Sung <personar95@naver.com>
2025-06-09 21:53:08 +0900

    포르잔 C++ 시작

---
+++ forouzan-cpp/RefPointerMngMems/ReferenceTestApp/ReferenceTestApp.cpp
@@ -0,0 +1,20 @@
+﻿// ReferenceTestApp.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;\
+
+int main()
+{
+		// Creation of reference relations
+		int score = 92;
+		int& rScore = score;
+		// 데이터 변수 사용
+		cout << "데이터 변수를 통해 값에 접근" << endl;
+		cout << "score: " << score << endl;
+		// 참조 변수 사용
+		cout << "참조 변수를 통해 값에 접근" << endl;
+		cout << "rScore: " << rScore;
+
+		return 0;
+}
